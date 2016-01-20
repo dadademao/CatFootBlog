b7f80895b320bd278efe0d1118fa6dcd9dfc4708
dadademao <18810950973@163.com>
2016-01-20 15:09:35 +0800

    加入滤镜demo
    用于测试gpu可执行度

---
+++ FilterTest/FilterTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FilterTest
+//
+//  Created by fuyuan on 1/19/16.
+//  Copyright © 2016 fuyuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FilterTest/FilterTest/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  FilterTest
+//
+//  Created by fuyuan on 1/19/16.
+//  Copyright © 2016 fuyuan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+- (void)drawLayer:(CALayer *)layer inContext:(CGContextRef)ctx {
+    [super drawLayer:layer inContext:ctx];
+}
+@end
